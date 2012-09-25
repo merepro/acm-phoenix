d3ab294be367d643987b85c7c46cabd730140ecc
Chris Manghane <paranoiacblack@gmail.com>
2012-09-24 22:08:17 -0700

    Finished Report Administration model.

--- acm_phoenix/admin/models.py
+++ acm_phoenix/admin/models.py
@@ -1,9 +1,12 @@
 from acm_phoenix.users.models import User
-from flask import flash
+from flask import flash, render_template, make_response, send_file
 from flask.ext.admin.contrib.sqlamodel import ModelView
 from flask.ext.admin import expose
 from flask.ext.admin.actions import action
 from flask.ext.admin.babel import gettext, lazy_gettext
+import xhtml2pdf.pisa as pisa
+import cStringIO as StringIO
+import zipfile
 
 class UserAdmin(ModelView):
     """
@@ -38,6 +41,29 @@ class ReportAdmin(ModelView):
     @action('copy', lazy_gettext('Make Paper Copy'))
     def generate_paper_copy(self, users):
         """
-        Turns user(s) information into pdf/package for download.
+        Turns user(s) information into zip package for download.
         """
-        flash(gettext('Generating Copies'))
+        zipdata = StringIO.StringIO()
+        zipped = zipfile.ZipFile(zipdata, "w")
+
+        for user_id in users:
+            user = User.query.get(user_id)
+
+            # For each user, render the membership form template with user-specific data
+            html = render_template("admin/report/membership_form_template.html", user=user)
+            result = StringIO.StringIO()
+
+            # Convert rendered xhtml into pdf file for specific user
+            pdf = pisa.CreatePDF(StringIO.StringIO(html.encode("utf_8")), dest=result)
+
+            if not pdf.err:
+                # Add this user's unique pdf to the zip archive
+                zipped.writestr(user.name + ".pdf", result.getvalue())
+                result.close()
+        zipped.close()
+        # seek(0) to make sure response is read from the beginning of the StringIO buffer
+        zipdata.seek(0)
+        # Create a zip attachment using send_file
+        response = send_file(zipdata, "application/zip", True, "membership_forms.zip")
+        return response
+
