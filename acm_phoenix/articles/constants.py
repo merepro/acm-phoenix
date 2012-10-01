d981430953328d69865768ae8fb3857e6f8c05f3
Chris Manghane <paranoiacblack@gmail.com>
2012-10-01 07:11:32 -0700

    Added some polish to the articles search page.

---
+++ acm_phoenix/articles/constants.py
@@ -0,0 +1,12 @@
+RECENCY="created"
+TITLE="title"
+CAT_NAME="slug"
+TAG_NAME="tags"
+AUTHOR_NAME="author"
+ORDER = {
+    RECENCY: "Recency",
+    TITLE: "Post Title",
+    CAT_NAME: "Category Name",
+    TAG_NAME: "Tag Name",
+    AUTHOR_NAME: "Author Name"
+}
--- acm_phoenix/articles/views.py
+++ acm_phoenix/articles/views.py
@@ -7,6 +7,7 @@ from acm_phoenix import app, db
 from acm_phoenix.users.models import User
 from acm_phoenix.articles.models import Post, Category, Tag
 from acm_phoenix.articles.forms import SearchForm
+from acm_phoenix.articles.constants import ORDER
 
 # Python implementation of Github Flavored Markdown
 from acm_phoenix.users.gfm import gfm
@@ -29,11 +30,81 @@ def timesince(date):
     format = "%b %d, %Y"
     return date.strftime(format)
 
+@app.template_filter('format_authors')
+def authors(author_ids):
+    """
+    Convert list of author ids into author names
+    """
+    if author_ids is None:
+        return ""
+    else:
+        ids = []
+        for author_id in author_ids.split(','):
+            ids.append(User.id == int(author_id))
+        authors = User.query.filter(or_(*ids)).all()
+        if authors is None:
+            return ""
+        else:
+            return "by " + ", ".join([author.name for author in authors])
+        
+
+@app.template_filter('format_query')
+def formatted_query(query):
+    """
+    Pretty print query
+    """
+    stripped_query = query.replace("%", "")
+    if len(stripped_query) == 0:
+        return ""
+    else:
+        return "with query like '" + stripped_query + "'"
+
+@app.template_filter('format_cats')
+def formatted_category(cat_ids):
+    """
+    Convert list of category ids into category slugs
+    """
+    if cat_ids is None:
+        return ""
+    else:
+        ids = []
+        for cat_id in cat_ids.split(','):
+            ids.append(Category.id == int(cat_id))
+        cats = Category.query.filter(or_(*ids)).all()
+        if cats is None:
+            return ""
+        else:
+            return "in " + ", ".join([cat.slug.title() for cat in cats])
+
+@app.template_filter('format_tags')
+def formatted_tag(tag_ids):
+    """
+    Convert list of tag ids into tag names
+    """
+    if tag_ids is None:
+        return ""
+    else:
+        ids = []
+        for tag_id in tag_ids.split(','):
+            ids.append(Tag.id == int(tag_id))
+        tags = Tag.query.filter(or_(*ids)).all()
+        if tags is None:
+            return ""
+        else:
+            return "with tags: " + ", ".join([tag.name.title() for tag in tags])
+
+@app.template_filter('format_order')
+def format_order(order):
+    if order is None:
+        return ""
+    else:
+        return "Ordered by " + ORDER[order]
+
 def valid_args(args):
     return args is not None and len(args) > 0
 
 def ilist_to_string(ilist):
-    return "".join([str(i) for i in ilist])
+    return ",".join([str(i) for i in ilist])
 
 # Routing rules
 @mod.route('/', methods=['GET', 'POST'])
@@ -43,9 +114,11 @@ def show_all():
     """
 
     # Request details
-    req_cats = None
+    req_cat = None
     req_auth = None
     req_tags = None
+    search_term = None
+    order = None
 
     # If there is no request, list posts by recency
     if len(request.args) == 0:
@@ -102,7 +175,7 @@ def show_all():
         args += "&order=" + form.order_by.data
 
         return redirect(url_for('articles.show_all') + args)
-    return render_template('articles/articles.html', posts=posts, form=form, cats=req_cats, authors=req_auth, tags=req_tags)
+    return render_template('articles/articles.html', posts=posts, form=form, query=search_term, cats=req_cat, authors=req_auth, tags=req_tags, order=order)
 
 @mod.route('/cat/<slug>/')
 def show_cat(slug):
