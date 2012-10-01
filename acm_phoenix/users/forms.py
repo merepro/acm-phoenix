3fee5a126ada92fb2c07d9c08deaf14c2f77ffcd
Chris Manghane <paranoiacblack@gmail.com>
2012-10-01 00:39:50 -0700

    Updated register forms to note github flavored markdown.

--- acm_phoenix/users/forms.py
+++ acm_phoenix/users/forms.py
@@ -8,7 +8,7 @@ class RegisterForm(Form):
     standing = SelectField(u'Class Standing', [Required()], choices=[('freshman', 'Freshman'), ('soph', 'Sophomore'), ('junior', 'Junior'), ('senior', 'Senior'), ('alum', 'Alumni'), ('grad', 'Graduate Student'), ('faculty', 'Faculty')])
     major = SelectField(u'Major', [Required()], choices=[('CS', 'CS (Computer Science)'), ('CE', 'CE (Computer Engineering)'), ('BI', 'BI (Business Informatics)'), ('Other', 'Other')])
     shirt_size = SelectField(u'T-Shirt Size', [Required()], choices=[('S', 'Small'), ('M', 'Medium'), ('L', 'Large'), ('XL', 'X-Large'), ('XXL', '2X-Large')])
-    description = TextAreaField(u'Describe yourself! (Markdown Allowed!)')
+    description = TextAreaField(u'Describe yourself! (Github Flavored Markdown Allowed!)')
     reg_only = SubmitField(u'<i class="icon-group"></i> Register Only')
     reg_and_pay = SubmitField(u'<i class="icon-credit-card"></i> Register and Pay Membership')
 
@@ -19,5 +19,5 @@ class EditForm(Form):
     standing = SelectField(u'Class Standing', [Required()], choices=[('freshman', 'Freshman'), ('soph', 'Sophomore'), ('junior', 'Junior'), ('senior', 'Senior'), ('alum', 'Alumni'), ('grad', 'Graduate Student'), ('faculty', 'Faculty')])
     major = SelectField(u'Major', [Required()], choices=[('CS', 'CS (Computer Science)'), ('CE', 'CE (Computer Engineering)'), ('BI', 'BI (Business Informatics)'), ('Other', 'Other')])
     shirt_size = SelectField(u'T-Shirt Size', [Required()], choices=[('S', 'Small'), ('M', 'Medium'), ('L', 'Large'), ('XL', 'X-Large'), ('XXL', '2X-Large')])
-    description = TextAreaField(u'Describe yourself! (Markdown Allowed!)')
+    description = TextAreaField(u'Describe yourself! (Github Flavored Markdown Allowed!)')
     
