33324c904fb59a941e64686c28b20c5a6d842b2a
Chris Manghane <paranoiacblack@gmail.com>
2012-10-26 00:35:09 -0700

    Added SVG fallback for mobile and older browsers with Svgeezy.

--- acm_phoenix/static/js/main.js
+++ acm_phoenix/static/js/main.js
@@ -1,1 +1,10 @@
+/**
+ * main.js - Javascript that must be ran on every page.
+ * Author: Chris Manghane (paranoiacblack)
+ */
 
+$(document).ready(function() {
+    // svgeezy.js is concantenated inside of plugin.js which is always
+    // loaded before this.
+    svgeezy.init('nocheck', 'png');
+});
