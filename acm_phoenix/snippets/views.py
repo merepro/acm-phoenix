24ad1d4d85f86cd379277a4e920744b8a77e9bbe
Chris Manghane <paranoiacblack@gmail.com>
2012-11-05 00:07:19 -0800

    Moved "Who are we?" section to about us page.

--- acm_phoenix/snippets/views.py
+++ acm_phoenix/snippets/views.py
@@ -2,6 +2,8 @@
 from flask import (Blueprint, request, render_template, flash, g, session,
                    redirect, url_for)
 
+from acm_phoenix.users.models import User
+
 mod = Blueprint('snippets', __name__, url_prefix='')
 
 # Routing rules
@@ -11,3 +13,11 @@ def show_mission_snippet():
     Displays ACM's Mission statement.
     """
     return render_template('snippets/mission.html')
+
+@mod.route('/about/')
+def show_about_us():
+    """
+    Displays information about the organization.
+    """
+    users = User.query.all()
+    return render_template('snippets/about.html', users=users)
