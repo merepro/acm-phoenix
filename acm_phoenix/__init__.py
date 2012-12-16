ad43e8ea47f0b1b447cfba029dd43b973f83f4b7
Chris Manghane <paranoiacblack@gmail.com>
2012-12-16 12:22:48 -0800

    Modified configuration to support different configuration versions and updated __init__.py to reflect changes.

--- acm_phoenix/__init__.py
+++ acm_phoenix/__init__.py
@@ -4,32 +4,37 @@ from flask.ext.paginate import Pagination
 
 from time import strftime
 
-app = Flask(__name__)
-app.config.from_object('config')
+db = SQLAlchemy()
 
-db = SQLAlchemy(app)
+# Define how to make configurable application
+def create_app(config_object, debug=False):
+    global db
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+    db.init_app(app)
+
+    return app
+
+def register_blueprints(app):
+    # Loading user modules.
+    from acm_phoenix.users.views import mod as usersModule
+    from acm_phoenix.articles.views import mod as articlesModule
+    from acm_phoenix.snippets.views import mod as snippetsModule
+    from acm_phoenix.admin import admin
+    
+    app.register_blueprint(usersModule)
+    app.register_blueprint(articlesModule)
+    app.register_blueprint(snippetsModule)
+    admin.init_app(app)
+
+app = create_app('config.DevelopmentConfig')
+register_blueprints(app)
+
+from acm_phoenix.users.models import User
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error), 404
-
-# Loading user modules.
-from acm_phoenix.users.views import mod as usersModule
-app.register_blueprint(usersModule)
-
-from acm_phoenix.articles.views import mod as articlesModule
-app.register_blueprint(articlesModule)
-
-from acm_phoenix.snippets.views import mod as snippetsModule
-app.register_blueprint(snippetsModule)
-
-from acm_phoenix.users.models import User
-from acm_phoenix.articles.models import Post, Tag, Category
-from acm_phoenix.articles.forms import SearchForm
-
-from acm_phoenix.admin import admin
-admin.init_app(app)
-
 
 @app.before_request
 def before_request():
@@ -46,16 +51,23 @@ def show_home():
     """
     Display home page to visitors and show front page articles.
     """
+    from acm_phoenix.articles.forms import SearchForm
+    from acm_phoenix.articles.models import Category, Post, Tag
     form = SearchForm()
+    
     frontpage_filter = Post.query.filter(Tag.name == "frontpage")
     posts = frontpage_filter.order_by("created DESC").all()
+
     cats = Category.query.all()
     tags = Tag.query.all()
+
     author_filter = User.query.filter(User.role < 2)
     authors = author_filter.order_by("name ASC").all()
+
     page = int(request.args.get('page')) if request.args.get('page') else 1
     pagination = Pagination(posts, per_page=4, total=len(posts),
                             page=page)
+
     return render_template('home.html', posts=posts, form=form, 
                            pagination=pagination, tags=tags, cats=cats,
                            authors=authors)
--- config.py
+++ config.py
@@ -1,29 +1,58 @@
+# Base configuration to be used on running server
+
+"""
+Note: Unless you are working on the server itself, you will not be able to run 
+it with a Config object. You must use a DevelopmentConfig or TestConfig.
+"""
 import os
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+class Config(object):
+    DEBUG = False
+    Testing = False
 
-ADMINS = frozenset(['acm.at.ucr+webmaster@gmail.com'])
-SECRET_KEY = 'MIIEpAIBAAKCAQEAwTs9CBbomFmsYmYzChcAT6FYm6szVtW2wlYaPRajiV7XvFaoZfBpDKDy2xsTaWkWdspR2juyyoVStJSuzLt7pIP7SX6ibDkbKtSthZJA5JAqhtvVF2BtR+yApLoM9hWEF4XQZDswMAGgYOBi+/C5FSqW/jPKoSaO+6dVgf+VKS9Nmp9G'
+    ADMINS = frozenset(['acm.at.ucr+webmaster@gmail.com'])
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'acm_phoenix.db')
-DATABASE_CONNECT_OPTIONS = {}
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'acm_phoenix.db')
+    DATABASE_CONNECT_OPTIONS = {}
 
-THREADS_PER_PAGE = 8
+    THREADS_PER_PAGE = 8
 
-CSRF_ENABLED=True
-CSRF_SESSION_KEY="TNSDOSJBjVSszvLMelsRafiw/z/flEnxh2LeZYMXuiSXxCAm32h9d6hkZpJRbE7Lpbk46sJmNLqRzCVInBOezAwOzR3Dv0FHx2s9kwIDAQABAoIBACB3Fnr8dlnafycNKrggQzId1qhY7EhDofAmzUPEQPe8kpyXJrXx3YR8qjD77JgCSv7sYTI8Y365RbsHXBMT0ONENX0UpK9wLMtWbk0J1JNSUYLU/olt7w5tgvOqOrFBzi6xkeC1PR"
+    CSRF_ENABLED = True
+    RECAPTCHA_USE_SSL = False
+    RECAPTCHA_OPTIONS = {'theme': 'white'}
 
-RECAPTCHA_USE_SSL = False
-RECAPTCHA_PUBLIC_KEY = '6LfDW9YSAAAAAAjSp9n2mS7G1bMwrYH7EESVOLQe'
-RECAPTCHA_PRIVATE_KEY = '6LfDW9YSAAAAAIJTT53vTrOzG5NdPhetB6Z8JLao'
-RECAPTCHA_OPTIONS = {'theme': 'white'}
+    # Giving server defined configuration default values.
+    SECRET_KEY = None
+    CSRF_SESSION_KEY = None
+    RECAPTCHA_PUBLIC_KEY = None
+    RECAPTCHA_PRIVATE_KEY = None
+    GOOGLE_CLIENT_ID = None
+    GOOGLE_CLIENT_SECRET = None
+    HOST_URL = None
+    WEPAY_ACCT_ID = None
+    WEPAY_ACC_TOK = None
+    WEPAY_IN_PROD = None
 
-GOOGLE_CLIENT_ID = '401399822645-a1015kkb76m6evpn3mhk3hr4voqejt2f.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET = 'TS6HarpynHCdSTesaRMlbaU_'
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SECRET_KEY = 'MIIEpAIBAAKCAQEAwTs9CBbomFmsYmYzChcAT6FYm6szVtW2wlYaPRajiV7XvFaoZfBpDKDy2xsTaWkWdspR2juyyoVStJSuzLt7pIP7SX6ibDkbKtSthZJA5JAqhtvVF2BtR+yApLoM9hWEF4XQZDswMAGgYOBi+/C5FSqW/jPKoSaO+6dVgf+VKS9Nmp9G'
 
-HOST_URL = 'http://localhost:5000'
+    CSRF_SESSION_KEY="TNSDOSJBjVSszvLMelsRafiw/z/flEnxh2LeZYMXuiSXxCAm32h9d6hkZpJRbE7Lpbk46sJmNLqRzCVInBOezAwOzR3Dv0FHx2s9kwIDAQABAoIBACB3Fnr8dlnafycNKrggQzId1qhY7EhDofAmzUPEQPe8kpyXJrXx3YR8qjD77JgCSv7sYTI8Y365RbsHXBMT0ONENX0UpK9wLMtWbk0J1JNSUYLU/olt7w5tgvOqOrFBzi6xkeC1PR"
+    RECAPTCHA_PUBLIC_KEY = '6LfDW9YSAAAAAAjSp9n2mS7G1bMwrYH7EESVOLQe'
+    RECAPTCHA_PRIVATE_KEY = '6LfDW9YSAAAAAIJTT53vTrOzG5NdPhetB6Z8JLao'
 
-WEPAY_ACCT_ID = 319493
-WEPAY_ACC_TOK = '6dd6802f8ebef4992308a0e4f7698c275781ac36854f9451127115d995d8cda7'
-WEPAY_IN_PROD = False
+    GOOGLE_CLIENT_ID = '401399822645-a1015kkb76m6evpn3mhk3hr4voqejt2f.apps.googleusercontent.com'
+    GOOGLE_CLIENT_SECRET = 'TS6HarpynHCdSTesaRMlbaU_'
+
+    HOST_URL = 'http://localhost:5000'
+
+    WEPAY_ACCT_ID = 319493
+    WEPAY_ACC_TOK = '6dd6802f8ebef4992308a0e4f7698c275781ac36854f9451127115d995d8cda7'
+    WEPAY_IN_PROD = False
+
+class TestingConfig(DevelopmentConfig):
+    DEBUG = False
+    TESTING = True
+    CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'tests/test.db')
