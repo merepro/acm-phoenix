d6c23e77efe54564fd4586e9dfb931cb7a781e69
Chris Manghane <paranoiacblack@gmail.com>
2012-12-28 22:28:47 -0800

    Added app initialization to reinit_db script since app creation is now delegated.

--- reinit_db.py
+++ reinit_db.py
@@ -1,4 +1,5 @@
-from acm_phoenix import db
-db.drop_all()
-db.create_all()
+from acm_phoenix import db, create_app
+app = create_app('config.DevelopmentConfig')
+db.drop_all(app=app)
+db.create_all(app=app)
 exit()
